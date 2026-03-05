a745d07fd104accf71f40a26c3570b09a51196c4
Felipe-Laskos <felipematheuslaskos@gmail.com>
2026-03-04 22:01:11 -0300

    Add initial folders setup

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { AutocadastroComponent } from './auth/autocadastro/autocadastro.component';
+import { HomeClienteComponent } from './cliente/home-cliente/home-cliente.component';
+import { HomeFuncionarioComponent } from './funcionario/home-funcionario/home-funcionario.component';
+import { CrudCategoriaComponent } from './funcionario/crud-categoria/crud-categoria.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'autocadastro', component: AutocadastroComponent },
+  { path: 'cliente', component: HomeClienteComponent },
+  { path: 'funcionario', component: HomeFuncionarioComponent },
+  { path: 'funcionario/categorias', component: CrudCategoriaComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' },
+];
