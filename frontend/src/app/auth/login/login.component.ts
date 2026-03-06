a7fbba2a1ec4f9d55b40d0f58ffcedea709b2db5
JessicaSouza <fatimajessica2@gmail.com>
2026-03-05 23:38:24 -0300

    Implementando as funcionalidades do RF002

--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    senha: ['', [Validators.required, Validators.pattern('^[0-9]{4}$')]]
+  });
 
+  constructor(private fb: FormBuilder, private router: Router) {}
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.value.email;
+      if (email?.includes('mario') || email?.includes('maria')) {
+        this.router.navigate(['/funcionario/home']);
+      } else {
+        this.router.navigate(['/cliente/home']);
+      }
+    }
+  }
 }
