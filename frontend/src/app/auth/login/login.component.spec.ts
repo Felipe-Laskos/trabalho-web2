cb80500589399db012ff02ab6db0c0c8947de4ba
JessicaSouza <fatimajessica2@gmail.com>
2026-03-05 23:38:24 -0300

    Implementando as funcionalidades do RF002

--- frontend/src/app/auth/login/login.component.spec.ts
+++ frontend/src/app/auth/login/login.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,16 +8,22 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        LoginComponent,
+        ReactiveFormsModule,
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve criar o componente de login', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve invalidar o formulário se estiver vazio', () => {
+    expect(component.loginForm.valid).toBeFalsy();
   });
 });
