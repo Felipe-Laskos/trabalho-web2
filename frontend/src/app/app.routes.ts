0f4d59b8c0c99ad4e7b5cd4274fd361f6b252f77
lauraklemba <laurakcordeiro2@gmail.com>
2026-03-14 19:11:20 -0300

    Adicionando tela mostrar-orcamento

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { AutocadastroComponent } from './auth/autocadastro/autocadastro.componen
 import { HomeClienteComponent } from './cliente/home-cliente/home-cliente.component';
 import { HomeFuncionarioComponent } from './funcionario/home-funcionario/home-funcionario.component';
 import { CrudCategoriaComponent } from './funcionario/crud-categoria/crud-categoria.component';
+import { MostrarOrcamentoComponent } from './cliente/mostrar-orcamento/mostrar-orcamento.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'autocadastro', component: AutocadastroComponent },
   { path: 'cliente', component: HomeClienteComponent },
+  { path: 'cliente/mostrar-orcamento', component: MostrarOrcamentoComponent},
   { path: 'funcionario', component: HomeFuncionarioComponent },
   { path: 'funcionario/categorias', component: CrudCategoriaComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
