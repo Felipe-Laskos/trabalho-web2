b650fd0d98bc6deb08dc72492477807c0e40d512
nvalbuquerque <nathalialyra@ufpr.br>
2026-03-15 18:47:14 -0300

    Correções merge

--- frontend/src/app/mocks/categoria.mock.ts
+++ frontend/src/app/mocks/categoria.mock.ts
@@ -4,60 +4,55 @@ export const mockCategoria: CategoriaEquipamento[] = [
     {
       id: 100,
       nome: "Laptop",
-      quantidade: 50,
+      quantidade: 10,
    },
 
    {
       id: 101,
       nome: "Ultrabook",
-      quantidade: 45,
+      quantidade: 5,
    },
 
     {
       id: 200,
       nome: "Desktop torre",
-      quantidade: 30,
+      quantidade: 12,
   },
 
   {
       id: 201,
       nome: "Desktop all in-one",
-      quantidade: 10,
+      quantidade: 2,
   },
 
    {
       id: 300,
       nome: "Impressora jato de tinta",
-      quantidade: 20,
+      quantidade: 5,
   },
 
   {
       id: 301,
       nome: "Impressora laser",
-      quantidade: 25,
+      quantidade: 8,
   },
 
    {
       id: 400,
-      nome: "Mouse óptico",
-      quantidade: 100,
+      nome: "Monitor LED",
+      quantidade: 2,
   },
 
    {
       id: 401,
       nome: "Mouse sem fio",
-      quantidade: 150,
+      quantidade: 0,
   },
 
   {
       id: 500,
       nome: "Teclado mecânico",
-      quantidade: 50,
+      quantidade: 0,
   },
-
-  {
-      id: 501,
-      nome: "Teclado de membrana",
-      quantidade: 50,
-  }
+  
 ];
