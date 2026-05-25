3d07d1b3359568e701d0ceffb05afd80a13dd9cc
Felipe-Laskos <felipematheuslaskos@gmail.com>
2026-05-25 18:45:15 -0300

    Fix HistoricoSolicitacaoRequestDTO

--- backend/manutencaoequipamentos/src/main/java/com/web/equipe5/manutencaoequipamentos/dto/request/HistoricoSolicitacaoRequestDTO.java
+++ backend/manutencaoequipamentos/src/main/java/com/web/equipe5/manutencaoequipamentos/dto/request/HistoricoSolicitacaoRequestDTO.java
@@ -3,7 +3,7 @@ package com.web.equipe5.manutencaoequipamentos.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record HistoricoSolicitacaoRequestDTO {
+public record HistoricoSolicitacaoRequestDTO(
     @NotNull(message = "O ID da solicitação é obrigatório.")
     Long solicitacaoId,
 
@@ -11,5 +11,4 @@ public record HistoricoSolicitacaoRequestDTO {
     String estadoFinal,
 
     String motivoRegra
-    
-}{}
+) {}
